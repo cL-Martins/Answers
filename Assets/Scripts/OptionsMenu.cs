20fbaaa324ebf16243f207d3e4325db0a39927e1
Bruneca <brunomartins616@hotmail.com>
2022-09-09 13:59:58 -0300

    Daltonic Complete
    Falta passar os dados para outra cena(Player Prefs)

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
-
+using Wilberforce;
 public class OptionsMenu : MonoBehaviour
 {
     public Toggle normal;
@@ -11,8 +11,25 @@ public class OptionsMenu : MonoBehaviour
     public Toggle deuteranopia;
     public Toggle tritanopia;
     public AudioMixer audioMixer;
+    public Colorblind daltonic;
     public void SetVolume (float volume){
-        audioMixer.SetFloat("Volume",volume);
+        audioMixer.SetFloat("Volume",volume);        
+    }
+    
+    void FixedUpdate()
+    {
+        if (normal.isOn){
+            daltonic.Type = 0;
+        }
+        else if (protanopia.isOn){
+            daltonic.Type = 1;
+        }
+        else if (deuteranopia.isOn){
+            daltonic.Type = 2;
+        }
+        else if (tritanopia.isOn){
+            daltonic.Type = 3;
+        }
     }
     
 }
