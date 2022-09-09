15328fadfca8707e57f8f6e5241dceaef6532470
ragster5 <alex_xandre93@hotmail.com>
2022-09-09 19:16:22 -0300

    Puzzle da chave escondida corrigido

--- Assets/Scripts/GavetaPuzzle.cs
+++ Assets/Scripts/GavetaPuzzle.cs
@@ -8,7 +8,7 @@ public class GavetaPuzzle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();
+        anim = GetComponentInParent<Animator>();
     }
 
     // Update is called once per frame
